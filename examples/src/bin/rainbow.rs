013e4ae941fc8ee7417fae5046cd149c03c08710
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-12-19 17:15:58 -0800

    Make the generic const N be the number of LEDs
    No more telling the user to multiply by 12.
    
    This is a breaking change.

--- examples/src/bin/rainbow.rs
+++ examples/src/bin/rainbow.rs
@@ -22,7 +22,7 @@ async fn main(_spawner: Spawner) {
     config.phase = Phase::CaptureOnFirstTransition;
     config.polarity = Polarity::IdleLow;
     let spi = Spi::new_txonly(p.SPI1, p.PIN_14, p.PIN_15, p.DMA_CH0, config);
-    let mut ws: Ws2812<_, Grb, { 12 * NUM_LEDS }> = Ws2812::new(spi);
+    let mut ws: Ws2812<_, Grb, NUM_LEDS> = Ws2812::new(spi);
 
     let mut data = [RGB8::default(); NUM_LEDS];
 
